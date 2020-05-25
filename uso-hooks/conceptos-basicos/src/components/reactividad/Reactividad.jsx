3e57fdaba2650983def1c542e06066dbdd5de488
BryanAnariba <43623601+BryanAnariba@users.noreply.github.com>
2020-05-25 00:35:50 -0600

    practicas

--- uso-hooks/conceptos-basicos/src/components/reactividad/Reactividad.jsx
+++ uso-hooks/conceptos-basicos/src/components/reactividad/Reactividad.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
+
 const reactividad = () => {
     return ( 
         <div className="container">
-            <h2>Works</h2>
+            <h2>Works Reactividad</h2>
         </div>
-     );
+    );
 }
- 
 export default reactividad;
