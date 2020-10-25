1f88b13215f8c4ab789aa093319b5e968d8b0600
Bryan Anariba <saariel115@gmail.com>
2020-10-25 00:58:51 -0600

    add docs

---
+++ Udemy-Fernando-Herrera-MERN/03-practica01-gifexpert/docs/precache-manifest.7a0615cb64fed542af5bc54f8d3ce704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5ed99187ca1356d2ab60572440ac00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be12ff121b2331465d9",
+    "url": "/static/css/main.8b8b1c5c.chunk.css"
+  },
+  {
+    "revision": "6a1c1fbf50a73828fc17",
+    "url": "/static/js/2.36a4dd95.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.36a4dd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be12ff121b2331465d9",
+    "url": "/static/js/main.9113d04e.chunk.js"
+  },
+  {
+    "revision": "a2d0539365f5385bd6ae",
+    "url": "/static/js/runtime-main.c00e1a77.js"
+  }
+]);
